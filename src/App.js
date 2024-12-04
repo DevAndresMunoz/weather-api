e5b28495ff01c161ea8abd0569a42ab6bfc0f90a
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-12-03 17:58:37 -0700

    progress, but still not working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import './App.css';
-import React from "react";
+import React, {Component} from "react";
 
 
-function App() {
-  const getCoordinatesFetch = async () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        lat: null,
+        lon: null
+    }
+}
+
+  componentDidMount() {
+    this.getCoordinatesWithFetch;
+  }
+
+  getCoordinatesFetch = async () => {
     let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
     let city = "Canton";
     let url = "http://api.openweathermap.org/geo/1.0/direct"
     
     try {
       let res = await fetch(`${url}?q=${city}&appid=${apiKey}`)
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`)
+      }
+
+      let data = await res.json();
+      console.log(data);
+      this.setState({ 
+          lat: data[0].lat, 
+          lon: data[0].lon,
+      })
+
     } catch (error) {
-      console.log(error.message);
+        console.log(error.message);
     }
   }
 
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <p>{`API KEY: ${process.env.REACT_APP_WEATHER_API_KEY}`}</p>
+        <p>{`Lat: ${this.state.lat}, Lon: ${this.state.lon}`}</p>
+      </div>
+    );
+  }
 
-  return (
-    <div className="App">
-      <h1>hello</h1>
-      <p>{getCoordinatesFetch}</p>
-    </div>
-  );
 }
 
 export default App;
