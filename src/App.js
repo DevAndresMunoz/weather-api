16907481720bd1a8e4ee75d52c7dbe3e6037996c
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-12-05 22:38:01 -0700

    created getWeatherWithFetch, still debugging

--- src/App.js
+++ src/App.js
@@ -6,38 +6,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        lat: null,
-        lon: null
+        apiKey: process.env.REACT_APP_WEATHER_API_KEY,
+        city: "Canton",
+        weatherData: [],
     }
 }
 
-  componentDidMount() {
-    this.getCoordinatesWithFetch();
+  async componentDidMount() {
+    const res = await this.getCoordinatesWithFetch();
+    const weather = await this.getWeatherWithFetch(res[0]);
+    this.setState({
+      weatherData: weather,
+    })
   }
 
   getCoordinatesWithFetch = async () => {
-    let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-    let city = "Canton";
     let url = "http://api.openweathermap.org/geo/1.0/direct"
     
     try {
-      let res = await fetch(`${url}?q=${city}&appid=${apiKey}`)
+      let res = await fetch(`${url}?q=${this.state.city}&appid=${this.state.apiKey}`)
       if (!res.ok) {
         throw new Error(`Error: ${res.status} - ${res.statusText}`)
       }
 
       let data = await res.json();
-      console.log(data);
-      this.setState({ 
-          lat: data[0].lat, 
-          lon: data[0].lon,
-      })
+      return data;
 
     } catch (error) {
         console.log(error.message);
     }
   }
 
+  getWeatherWithFetch = async (data) => {
+    let url = "https://api.openweathermap.org/data/2.5/weather";
+    try {
+      let res = await fetch(`${url}?lat=${this.state.lat}&lon=${this.state.lon}&appid=${this.state.apiKey}`)
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`)
+      }
+
+      let weather = await res.json();
+      console.log(weather);
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+  }
 
   
   render() {
@@ -45,7 +60,7 @@ class App extends Component {
       <div className="App">
         <h1>Weather App</h1>
         <p>{`API KEY: ${process.env.REACT_APP_WEATHER_API_KEY}`}</p>
-        <p>{`Lat: ${this.state.lat}, Lon: ${this.state.lon}`}</p>
+        <p>{`Lat: `}</p>
       </div>
     );
   }
